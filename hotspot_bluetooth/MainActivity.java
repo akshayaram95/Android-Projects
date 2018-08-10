1d4da6fa0d6d5ceb1ec7ee7e6b5eb64005a8478d
akshayaram95 <akshayaram95@gmail.com>
2018-08-10 11:56:51 -0700

    Add files via upload

---
+++ hotspot_bluetooth/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.akshaya.hotspot;
+
+import android.content.Context;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.lang.reflect.Method;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b_on;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b_on = (Button) findViewById(R.id.b_on);
+
+
+
+        b_on.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isApOn(MainActivity.this);
+                configApState(MainActivity.this);
+            }
+        });
+    }
+
+    //check if hotspot is on
+    public static boolean isApOn(Context context) {
+        WifiManager wifimanager = (WifiManager) context.getSystemService(context.WIFI_SERVICE);
+        try {
+            Method method = wifimanager.getClass().getDeclaredMethod("isWifiApEnabled");
+            method.setAccessible(true);
+            return (Boolean) method.invoke(wifimanager);
+        }
+        catch (Throwable ignored) {}
+        return false;
+    }
+
+    //toggle hotspot status [if 'on', then turn 'off' and vice versa]
+    public static boolean configApState(Context context) {
+        WifiManager wifimanager = (WifiManager) context.getSystemService(context.WIFI_SERVICE);
+        WifiConfiguration wificonfiguration = null;
+        try {
+            // if WiFi is on, turn it off
+            if(isApOn(context)) {
+                wifimanager.setWifiEnabled(false);
+            }
+            Method method = wifimanager.getClass().getMethod("setWifiApEnabled", WifiConfiguration.class, boolean.class);
+            method.invoke(wifimanager, wificonfiguration, !isApOn(context));
+            return true;
+        }
+        catch (Exception e) {
+
+        }
+        return false;
+    }
+}
